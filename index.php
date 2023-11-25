c6f84b8534efc87e7eda4884e2371e3403280351
wahyuandriono10193 <wahyuandriono10193@gmail.com>
2023-11-24 18:30:19 -0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello world!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam rerum consectetur, neque fugiat, impedit iusto provident laborum molestias iste voluptate doloribus nemo cumque harum soluta reiciendis optio fuga culpa! Aliquam?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
